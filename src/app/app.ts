474a8da4096efb7accb48180fa49a9c082df5f6d
Gautami-Vikhe <gautamivikhe09@gmail.com>
2026-08-31 11:39:36 +0530

    Add task creation and deletion with data binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,27 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header],
+  imports: [RouterOutlet,Header,FormsModule,CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  taskName='';
+  tasks:string[]=[];
+
+  addTask(){
+    if(this.taskName.trim()){
+      this.tasks.push(this.taskName);
+      this.taskName='';
+    }
+  }
+  removeTask(index:number){
+    this.tasks.splice(index,1);
+  }
   protected readonly title = signal('task-manager');
 }
