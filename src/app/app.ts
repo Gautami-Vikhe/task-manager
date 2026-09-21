3374185d20e8c0decb5f589a5a9d6012ca9a1ae9
Gautami-Vikhe <gautamivikhe09@gmail.com>
2026-09-21 14:29:14 +0530

    Add routing with Home and Tasks pages,restyle header and home page

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,7 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { task } from './task';
+
 
 
 @Component({
@@ -12,22 +12,6 @@ import { task } from './task';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit {
-  taskName='';
-
-  constructor(public task: task){
-    console.log('Constructor called');
-  }
-
-  ngOnInit(){
-    console.log('App component initialized.Current tasks:',this.task.tasks);
-  }
-
-  onAddTask(){
-    this.task.addTask(this.taskName);
-    this.taskName=''
-  }
-  onHeaderTitleClicked(){
-    console.log('Header title was clicked!');
-  }
+export class App {
+  
 }
---
+++ src/app/tasks/tasks.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { task } from '../task';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  imports: [FormsModule,CommonModule],
+  selector: 'app-tasks',
+  styleUrl: './tasks.css',
+  templateUrl: './tasks.html',
+})
+export class Tasks implements OnInit {
+  taskName='';
+
+  constructor(public task: task){
+    console.log('Constructor called');
+  }
+
+  ngOnInit(){
+    console.log('App component initialized.Current tasks:',this.task.tasks);
+  }
+
+  onAddTask(){
+    this.task.addTask(this.taskName);
+    this.taskName=''
+  }
+ 
+}
